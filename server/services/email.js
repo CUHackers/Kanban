5706b245cd35c45c97e18d9349cd4a156a97fa64
Josh Hunt <joshhruns@gmail.com>
2020-09-06 21:10:41 -0400

    Added email template

--- server/services/email.js
+++ server/services/email.js
@@ -1,27 +1,44 @@
+const path = require('path');
 var nodemailer = require("nodemailer");
+var hbs = require('nodemailer-express-handlebars');
 
 // create reusable transporter object using the default SMTP transport
 var transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: process.env.EMAIL_PORT,
-    secure: true, // use SSL
+    secure: true,
     auth: {
-        user: process.env.EMAIL_USER, // generated ethereal user
-        pass: process.env.EMAIL_PASS, // generated ethereal password
-    },
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
+    }
 });
 
 var controller = {};
 
 controller.transporter = transporter;
 
+viewPath = path.resolve("server/views/");
+
+transporter.use('compile', hbs({
+    viewEngine: {
+        extName: '.handlebars',
+        partialsDir: viewPath,
+        layoutsDir: viewPath,
+        defaultLayout: false,
+    },
+    viewPath: viewPath
+}));
+
 controller.sendVerificationEmail = function(email, token, callback) {
     // send mail with defined transport object
     transporter.sendMail({
         from: '"Fred Foo 👻" <foo@example.com>', // sender address
         to: email, // list of receivers
-        subject: 'Verify Your Email ✔', // Subject line
-        text: 'Please verify your account by clicking the link:\n' + process.env.ROOT_URL + '/verify/' + token + '\n' // 
+        subject: 'CUHackit - Verify Your Email ✔',
+        template: 'index',
+        context: {
+            verifyLink: process.env.ROOT_URL + '/verify/' + token
+        }
     }, function(err, info){
         if (err) { 
             console.log(err); 
@@ -30,7 +47,6 @@ controller.sendVerificationEmail = function(email, token, callback) {
             callback(err, info)
         }
     });
-
-}
+};
 
 module.exports = controller;
